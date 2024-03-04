62813c62002d62bd9d417eaebd798907465b99b5
calltella <yuichi.asa@gmail.com>
2024-03-04 15:55:43 +0900

    production bat fix

--- bat/production/production_attachment_import.sh
+++ bat/production/production_attachment_import.sh
@@ -3,5 +3,16 @@
 #
 # 開発環境から起動して本番環境の添付ファイル群をコピー
 #
-cd ~/apline-laravel10/storage/app/apline
-zip ~/Docker-Laravel-Pgsql/export/archive2023 -r 2023
+
+
+arg1=$1
+
+USER_DIRECTORY=~
+
+ATTACH_DIRECTORY="${USER_DIRECTORY}/apline-laravel10/storage/app/apline/${arg1}"
+
+#cd "${USER_DIRECTORY}/apline-laravel10/storage/app/apline"
+
+#echo "${USER_DIRECTORY}/apline-laravel10/storage/app/apline"
+echo $ATTACH_DIRECTORY
+zip "${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/archive${arg1}.zip" -r $ATTACH_DIRECTORY

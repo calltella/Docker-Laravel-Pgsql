9b14c5cd3095f0c3bca99b7983474cbbde83a4f3
calltella <yuichi.asa@gmail.com>
2024-03-07 16:14:18 +0900

    AL2023 change

---
+++ bat/docker/container_start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+# ユーザーディレクトリを取得
+USER_DIRECTORY=~
+
+# envファイルから環境変数を読込
+source "${USER_DIRECTORY}/Docker-Laravel-Pgsql/.env"
+
+# コンテナのIDを取得
+CONTAINER_ID=$(docker ps -q --filter name=$DATABASE_CONTAINER_NAME)
+
+# コンテナが起動しているか確認
+if [ -z "$CONTAINER_ID" ]; then
+  echo "Container $DATABASE_CONTAINER_NAME is not running."
+  /usr/local/bin/docker-compose -f "${USER_DIRECTORY}/Docker-Laravel-Pgsql/docker-compose.yml" up -d
+  exit 1  # 終了コード 1 でスクリプトを終了
+else
+  echo "Container $DATABASE_CONTAINER_NAME is running with ID: $CONTAINER_ID"
+fi
+
+
+
---
+++ bat/local/developmentBackup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# ユーザーディレクトリを取得
+USER_DIRECTORY=~
+
+# envファイルから環境変数を読込
+source "${USER_DIRECTORY}/Docker-Laravel-Pgsql/.env"
+
+# コンテナIDを取得
+CONTAINER_ID=$(docker ps -q --filter name="${DATABASE_CONTAINER_NAME}")
+
+# コンテナが起動しているか確認
+if [ -z "$CONTAINER_ID" ]; then
+  echo "Container $DATABASE_CONTAINER_NAME is not running."
+  exit 1  # 終了コード 1 でスクリプトを終了
+else
+  echo "Container $DATABASE_CONTAINER_NAME is running with ID: $CONTAINER_ID"
+fi
+
+# 日付と時間をファイルネームに
+CURRENT_DATE=$(date +'%Y%m%d-%H%M%S')
+
+# セーブパスの結合
+SAVEPATH="${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/pgsql"
+
+# ファイルネームを作成
+SAVEFILE="development-dbdump-${CURRENT_DATE}"
+
+# データベースをダンプ
+# pg_dumpがファイルはバイナリファイルなので圧縮できない
+docker exec $CONTAINER_ID bash -c 'set PGPASSWORD="${PGDUMP_PASSWORD}"'
+docker exec $CONTAINER_ID bash -c "/usr/local/bin/pg_dump -U postgres -c --if-exists -d development -t phpipam_subnet_table -t phpipam_display_information -f /tmp/pgsql/${SAVEFILE}.sql"
+
+# 過去３日以上経過したファイルは削除
+sudo find $SAVEPATH -type f -daystart -mtime +3 -exec rm {} \;
+
+
+

fad531d6ff079ec8585b29e36a40b5645b8f5d64
calltella <yuichi.asa@gmail.com>
2024-07-21 14:59:53 +0900

    Merge remote changes

--- bat/production/production_attachment_import.sh
+++ bat/production/production_attachment_import.sh
@@ -18,12 +18,10 @@ USER_DIRECTORY=~
 
 ATTACH_DIRECTORY="${USER_DIRECTORY}/apline_laravel10/storage/app/apline/${arg1}"
 
-FILE_STORE_DIRECTORY="${USER_DIRECTORY}/apline_laravel10/storage/app/filestore/${arg1}"
-#cd "${USER_DIRECTORY}/apline-laravel10/storage/app/apline"
+FILE_STORE_DIRECTORY="${USER_DIRECTORY}/apline_laravel10/storage/app/filestore/"
 
-#echo "${USER_DIRECTORY}/apline-laravel10/storage/app/apline"
 echo $ATTACH_DIRECTORY
 zip "${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/archive${arg1}.zip" -r $ATTACH_DIRECTORY
 
 echo $FILE_STORE_DIRECTORY
-zip "${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/archive/filestore.zip" -r $FILE_STORE_DIRECTORY
+zip "${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/filestore.zip" -r $FILE_STORE_DIRECTORY

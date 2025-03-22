453472afbe5c7270b411b9bfb7dfa33e6257255b
calltella <yuichi.asa@gmail.com>
2025-03-22 10:38:23 +0900

    production-db-copy fix.

--- bat/local/production-db-copy.sh
+++ bat/local/production-db-copy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Dockerが起動している事が前提
-# 毎日１２時に起動
-# FJ001からバックアップファイルをコピーしてきて
-# 開発環境のproductionデータベースにインポート
+# 本番環境のデータベースを開発環境にコピーする
+# Cronで app:restore-database-command を実行することで
+# Dropboxからファイルをダウンロードしproductionデータベースにインポート
 # 特定のテーブルのみdevelopmentデータベースにコピー
+#
 
 # 添付ファイルの最新化
 
 set -e
 
 # コマンドライン引数の取得
-arg1=$1
+#arg1=$1
 
 # 引数がなかった場合は今年をセット
-if [ -z "$arg1" ]; then
-  BACKUP_DATE=$(date -d 'yesterday' '+%Y%m%d')
-else
-  BACKUP_DATE=$arg1
-fi
+#if [ -z "$arg1" ]; then
+#  BACKUP_DATE=$(date -d 'yesterday' '+%Y%m%d')
+#else
+#  BACKUP_DATE=$arg1
+#fi
 
 # ユーザーディレクトリを取得
 USER_DIRECTORY=~
@@ -28,7 +28,7 @@ source "${USER_DIRECTORY}/Docker-Laravel-Pgsql/.env"
 
 # コンテナのIDを取得
 DATABASE_CONTAINER_ID=$(docker ps -q --filter name=$DATABASE_CONTAINER_NAME)
-LARAVEL_CONTAINER_ID=$(docker ps -q --filter name=$LARAVEL_CONTAINER_NAME)
+#LARAVEL_CONTAINER_ID=$(docker ps -q --filter name=$LARAVEL_CONTAINER_NAME)
 
 
 # コンテナが起動しているか確認
@@ -40,31 +40,31 @@ else
 fi
 
 # バックアップファイルを配置するディレクトリ
-BACKUP_DIRECTORY="${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/pgsql"
+#BACKUP_DIRECTORY="${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/pgsql"
 
 # ディレクトリが存在しない場合は作成
-if [ ! -d "$BACKUP_DIRECTORY" ]; then
-    mkdir -p "$BACKUP_DIRECTORY"
-    echo "Directory created: $BACKUP_DIRECTORY"
-fi
+#if [ ! -d "$BACKUP_DIRECTORY" ]; then
+#    mkdir -p "$BACKUP_DIRECTORY"
+#    echo "Directory created: $BACKUP_DIRECTORY"
+#fi
 
 # 不要なファイルを削除
-find "${BACKUP_DIRECTORY}" -name "production-dbdump-*.sql" -type f -exec rm -f {} \; -o -name "production-dbdump-*.zip" -type f -exec rm -f {} \;
+#find "${BACKUP_DIRECTORY}" -name "production-dbdump-*.sql" -type f -exec rm -f {} \; -o -name "production-dbdump-*.zip" -type f -exec rm -f {} \;
 
 # 本番環境からファイルをコピー
-scp "preserver30:${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/DailyBackup/production-dbdump-*.sql" "${BACKUP_DIRECTORY}"
+#scp "preserver30:${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/DailyBackup/production-dbdump-*.sql" "${BACKUP_DIRECTORY}"
 
 # 昨日日付のファイルを選択
-BACKUP_FILE=$(find "${BACKUP_DIRECTORY}" -type f -name "*${BACKUP_DATE}*" -exec basename {} \;)
+#BACKUP_FILE=$(find "${BACKUP_DIRECTORY}" -type f -name "*${BACKUP_DATE}*" -exec basename {} \;)
 
 # バックアップファイルをレストア
-docker exec $DATABASE_CONTAINER_ID bash -c "psql -U postgres -d production -f /tmp/pgsql/${BACKUP_FILE}"
+#docker exec $DATABASE_CONTAINER_ID bash -c "psql -U postgres -d production -f /tmp/pgsql/${BACKUP_FILE}"
 
 # table development database copy
 copy_table_to_development() {
-    TABLE=$1
-    docker exec $DATABASE_CONTAINER_ID bash -c "pg_dump -c --if-exists -U postgres -t $TABLE production > /tmp/pgsql/production_$TABLE.dump"
-    docker exec $DATABASE_CONTAINER_ID bash -c "psql -U postgres -d development -c 'TRUNCATE TABLE $TABLE;' && psql -U postgres -d development < /tmp/pgsql/production_$TABLE.dump"
+  TABLE="$1"
+  docker exec "$DATABASE_CONTAINER_ID" bash -c "pg_dump -c -U postgres -t \"$TABLE\" production > /tmp/pgsql/production_\"$TABLE\".dump"
+  docker exec "$DATABASE_CONTAINER_ID" bash -c "psql -U postgres -d development -c 'TRUNCATE TABLE \"$TABLE\" CASCADE;' && psql -U postgres -d development < /tmp/pgsql/production_\"$TABLE\".dump"
 }
 
 copy_table_to_development "migrate_pos_helpdesk_daily_reports"
@@ -79,16 +79,16 @@ copy_table_to_development "migrate_local_file_storage_file_history"
 
 
 # 不要なファイルを削除
-docker exec $DATABASE_CONTAINER_ID bash -c "rm -f /tmp/pgsql/*.dump"
+#docker exec $DATABASE_CONTAINER_ID bash -c "rm -f /tmp/pgsql/*.dump"
 
 # dropbox用バックアップファイルコピー
-BACKUP_FILE_NAME="${BACKUP_FILE%.*}"
-HOST_PATH="/home/ec2-user/Docker-Laravel-Pgsql/export/pgsql/${BACKUP_FILE_NAME}.sql"
-TARGET_PATH="/home/ec2-user/apline_laravel10/storage/app/backup"
-cp $HOST_PATH "${TARGET_PATH}/${BACKUP_FILE_NAME}.sql"
+#BACKUP_FILE_NAME="${BACKUP_FILE%.*}"
+#HOST_PATH="/home/ec2-user/Docker-Laravel-Pgsql/export/pgsql/${BACKUP_FILE_NAME}.sql"
+#TARGET_PATH="/home/ec2-user/apline_laravel10/storage/app/backup"
+#cp $HOST_PATH "${TARGET_PATH}/${BACKUP_FILE_NAME}.sql"
 
 # バックアップファイルの圧縮
-zip "${TARGET_PATH}/${BACKUP_FILE_NAME}.zip" "${TARGET_PATH}/${BACKUP_FILE_NAME}.sql"
+#zip "${TARGET_PATH}/${BACKUP_FILE_NAME}.zip" "${TARGET_PATH}/${BACKUP_FILE_NAME}.sql"
 
 
 

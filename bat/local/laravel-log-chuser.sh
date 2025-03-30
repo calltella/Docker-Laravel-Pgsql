69a38ab411a011f81366744b6474b8290269ebef
calltella <yuichi.asa@gmail.com>
2025-03-30 12:06:28 +0900

    log chown user batch add.

---
+++ bat/local/laravel-log-chuser.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Laravelログをローテートするとユーザーがrootになるので対策
+#
+
+set -e
+
+# ユーザーディレクトリを取得
+USER_DIRECTORY=~
+
+# envファイルから環境変数を読込
+source "${USER_DIRECTORY}/Docker-Laravel-Pgsql/.env"
+
+LARAVEL_CONTAINER_ID=$(docker ps -q --filter name=$LARAVEL_CONTAINER_NAME)
+
+# Laravelログの所有者を変更
+docker exec -it $CONTAINER_ID chown -R nginx:nginx /var/www/html/storage/logs/
+
+# ngixnログの所有者を変更
+docker exec -it $CONTAINER_ID chown -R nginx:nginx /var/log
+
+
+
--- bat/local/production-db-copy.sh
+++ bat/local/production-db-copy.sh
@@ -76,6 +76,9 @@ copy_table_to_development "migrate_apline_users_list"
 copy_table_to_development "migrate_fresta_ipadress_thirdoctet"
 copy_table_to_development "migrate_local_file_storage_store"
 copy_table_to_development "migrate_local_file_storage_file_history"
+copy_table_to_development "migrate_fresta_ping_exec_values"
+copy_table_to_development "migrate_store_information"
+copy_table_to_development "migrate_store_device_fp1_setup_info"
 
 
 # 不要なファイルを削除

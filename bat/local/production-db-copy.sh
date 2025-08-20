8ca573a9023fb40cdaaa7e278a8c9cbd7f27f6b1
calltella <yuichi.asa@gmail.com>
2025-08-20 16:40:14 +0900

    db copy fix.

--- bat/local/production-db-copy.sh
+++ bat/local/production-db-copy.sh
@@ -6,19 +6,7 @@
 # 特定のテーブルのみdevelopmentデータベースにコピー
 #
 
-# 添付ファイルの最新化
-
 set -e
-
-# コマンドライン引数の取得
-#arg1=$1
-
-# 引数がなかった場合は今年をセット
-#if [ -z "$arg1" ]; then
-#  BACKUP_DATE=$(date -d 'yesterday' '+%Y%m%d')
-#else
-#  BACKUP_DATE=$arg1
-#fi
 
 # ユーザーディレクトリを取得
 USER_DIRECTORY=~
@@ -28,8 +16,6 @@ source "${USER_DIRECTORY}/Docker-Laravel-Pgsql/.env"
 
 # コンテナのIDを取得
 DATABASE_CONTAINER_ID=$(docker ps -q --filter name=$DATABASE_CONTAINER_NAME)
-#LARAVEL_CONTAINER_ID=$(docker ps -q --filter name=$LARAVEL_CONTAINER_NAME)
-
 
 # コンテナが起動しているか確認
 if [ -z "$DATABASE_CONTAINER_ID" ]; then
@@ -38,29 +24,6 @@ if [ -z "$DATABASE_CONTAINER_ID" ]; then
 else
   echo "Container $DATABASE_CONTAINER_NAME is running with ID: $DATABASE_CONTAINER_ID"
 fi
-
-# バックアップファイルを配置するディレクトリ
-#BACKUP_DIRECTORY="${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/pgsql"
-
-# ディレクトリが存在しない場合は作成
-#if [ ! -d "$BACKUP_DIRECTORY" ]; then
-#    mkdir -p "$BACKUP_DIRECTORY"
-#    echo "Directory created: $BACKUP_DIRECTORY"
-#fi
-
-# 不要なファイルを削除
-#find "${BACKUP_DIRECTORY}" -name "production-dbdump-*.sql" -type f -exec rm -f {} \; -o -name "production-dbdump-*.zip" -type f -exec rm -f {} \;
-
-# 本番環境からファイルをコピー
-#scp "preserver30:${USER_DIRECTORY}/Docker-Laravel-Pgsql/export/DailyBackup/production-dbdump-*.sql" "${BACKUP_DIRECTORY}"
-
-# 昨日日付のファイルを選択
-#BACKUP_FILE=$(find "${BACKUP_DIRECTORY}" -type f -name "*${BACKUP_DATE}*" -exec basename {} \;)
-
-# バックアップファイルをレストア
-#docker exec $DATABASE_CONTAINER_ID bash -c "psql -U postgres -d production -f /tmp/pgsql/${BACKUP_FILE}"
-
-# table development database copy
 
 
 sync_table_to_development() {
@@ -87,7 +50,6 @@ sync_table_to_development "migrate_apline_file_store"
 sync_table_to_development "migrate_apline_pulldown_list"
 sync_table_to_development "migrate_apline_users_list"
 
-
 sync_table_to_development "migrate_apline_subsystem_lists"
 sync_table_to_development "migrate_apline_classification_lists"
 sync_table_to_development "migrate_apline_business_lists"
@@ -112,6 +74,9 @@ sync_table_to_development "migrate_store_device_fp1_ping_log"
 
 sync_table_to_development "migrate_pos_helpdesk_daily_reports"
 
+sync_table_to_development "migrate_scrape_cvcf_status"
+sync_table_to_development "migrate_scrape_cvcf_settings"
+
 # インデックスの再作成
 echo "Recreating index on migrate_apline_base_model..."
 docker exec "$DATABASE_CONTAINER_ID" bash -c "psql -U postgres -d development -c \"
@@ -122,18 +87,3 @@ CREATE INDEX pgroonga_nfkc100_unify_kana_index
 \""
 
 echo "Index recreated successfully."
-
-# 不要なファイルを削除
-#docker exec $DATABASE_CONTAINER_ID bash -c "rm -f /tmp/pgsql/*.dump"
-
-# dropbox用バックアップファイルコピー
-#BACKUP_FILE_NAME="${BACKUP_FILE%.*}"
-#HOST_PATH="/home/ec2-user/Docker-Laravel-Pgsql/export/pgsql/${BACKUP_FILE_NAME}.sql"
-#TARGET_PATH="/home/ec2-user/apline_laravel10/storage/app/backup"
-#cp $HOST_PATH "${TARGET_PATH}/${BACKUP_FILE_NAME}.sql"
-
-# バックアップファイルの圧縮
-#zip "${TARGET_PATH}/${BACKUP_FILE_NAME}.zip" "${TARGET_PATH}/${BACKUP_FILE_NAME}.sql"
-
-
-

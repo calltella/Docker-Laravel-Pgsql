723fd384059bd3f505a7912111f9a3f124a43b0b
calltella <yuichi.asa@gmail.com>
2026-07-09 15:16:55 +0900

    起動ユーザー修正

--- bat/local/laravel-log-chuser.sh
+++ bat/local/laravel-log-chuser.sh
@@ -19,9 +19,9 @@ LARAVEL_CONTAINER_ID=$(docker ps -q --filter name=$LARAVEL_CONTAINER_NAME)
 echo "Container $LARAVEL_CONTAINER_NAME is running with ID: $LARAVEL_CONTAINER_ID"
 
 # Laravelログの所有者を変更
-docker exec $LARAVEL_CONTAINER_ID chown -R nginx:nginx /var/www/html/storage/logs/
+docker exec $LARAVEL_CONTAINER_ID chown -R 1000:1000 /var/www/html/laravel/storage/logs/
 echo "Laravel logs ownership changed to nginx:nginx"
 
 # Nginxログの所有者を変更
-docker exec $LARAVEL_CONTAINER_ID chown -R nginx:nginx /var/log
+docker exec $LARAVEL_CONTAINER_ID chown -R 1000:1000 /var/log
 echo "Nginx logs ownership changed to nginx:nginx"
